c959e4c882aae56053837a93d860a068a14da325
Charlie Sharpsteen <source@sharpsteen.net>
2012-02-15 22:36:10 -0800

    Write sea surface elevation into XMDF file
    At this point, the program is capable of transferring SSE predictions from
    ADCIRC NetCDF files and storing them in an XMDF file.
    
    Things that need to be fixed:
    
      - Time steps need to be converted to julian dates. This is a big one.
    
      - The metadata group, `PROPERTIES`, is not being created.

--- adcirc2xmdf.f90
+++ adcirc2xmdf.f90
@@ -10,13 +10,13 @@ program adcirc2xmdf
   integer, dimension(NF90_MAX_VAR_DIMS):: dims
   integer:: n_tstep, n_node
   double precision, dimension(:,:), allocatable:: slice
+  double precision, dimension(:), allocatable:: times
 
   ! NC_FID = Integer id value corresponding to an opened netCDF file.
   ! NC_VID = Integer id value corresponding to a variable in a netCDF file.
   integer:: NC_FID, NC_VID
 
-  integer:: XF_FID
-
+  integer:: XF_FID, XF_GID, XF_VID
   ! XMDF subroutine flags require 2 byte logicals for some strange reason
   logical(kind=2):: XF_FLAG
 
@@ -58,7 +58,35 @@ program adcirc2xmdf
   end if
 
 
-  ! Find sea surface elevation variable.
+  ! Create top-level group in XMDF file, will be named 'Datasets'
+  call xf_create_generic_group(XF_FID, MULTI_DATASET_LOCATION, XF_GID, stat)
+
+  ! Start a new dataset for sea surface elevation.
+  call xf_create_scalar_dataset(XF_GID, 'Water Surface Elevation (63)', 'm', TS_SECONDS, -1, XF_VID, stat)
+
+  ! Find sea surface elevation time steps
+  stat = nf90_inq_varid(NC_FID, 'time', NC_VID)
+  if(stat /= NF90_NOERR) then
+    write(*,*) nf90_strerror(stat)
+    stop "Could not recover time steps!"
+  end if
+
+
+  stat = nf90_inquire_variable(NC_FID, NC_VID, dimids = dims)
+  if(stat /= NF90_NOERR) then
+    write(*,*) nf90_strerror(stat)
+    stop "Could not recover dimensions of time step vector!"
+  end if
+
+  stat = nf90_inquire_dimension(NC_FID, dims(1), len = n_tstep)
+  allocate(times(n_tstep))
+
+
+  ! FIXME: The timesteps need to be cast into __julian dates__!
+  stat = nf90_get_var(NC_FID, NC_VID, times, (/1/), (/n_tstep/))
+
+
+  ! Find sea surface elevation variable in NetCDF output.
   stat = nf90_inq_varid(NC_FID, 'zeta', NC_VID)
   if(stat /= NF90_NOERR) then
     write(*,*) nf90_strerror(stat)
@@ -83,13 +111,13 @@ program adcirc2xmdf
   allocate(slice(1,n_node))
 
 
-  do i = 1, 10
-    stat = nf90_get_var(NC_FID, NC_VID, slice, (/1, i/), (/10, 1/))
+  do i = 1, n_tstep
+    stat = nf90_get_var(NC_FID, NC_VID, slice, (/1, i/), (/n_node, 1/))
     if(stat /= NF90_NOERR) then
       write(*,*) nf90_strerror(stat)
       stop "Could not extract data!"
     end if
-    write(*,"(10F8.4)") (slice(1,j), j = 1, 10)
+    call xf_write_scalar_timestep(XF_VID, times(i), n_node, real(slice(1,:), 4), stat)
   end do
 
 
@@ -99,9 +127,10 @@ program adcirc2xmdf
     stop "WARNING! Errors occurred while closing the netCDF file!"
   end if
 
+  call xf_close_group(XF_GID, stat)
   call xf_close_file(XF_FID, stat)
   if ( stat < 0 ) stop 'WARNING! Errors occurred while closing the XMDF file!'
 
-  deallocate(slice)
+  deallocate(slice, times)
 stop
 end program adcirc2xmdf
